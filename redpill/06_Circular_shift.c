470d610421afe1dd2649dda235b5665f07ad11e8
triton0305 <triton0305@gmail.com>
2026-08-12 20:29:19 +0900

    Refactor rotate logic and improve type safety

--- redpill/06_Circular_shift.c
+++ redpill/06_Circular_shift.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define max_rot 32
+#define BIT_WIDTH 32
 
 typedef struct{
     uint32_t input;
-    int index;
-    int rot_cnt;
-}test;
+    unsigned int index;
+    unsigned int rot_cnt;
+}rotateTest;
 
 void printInit (void);
 void printBinary(uint32_t data);
-void printPrologue(test data);
-void rotateRight(test* data);
-void rotateLeft(test* data);
+void printLogue(rotateTest data);
+void leftRun(rotateTest*data);
+void rightRun(rotateTest*data);
 
+rotateTest leftShifter(rotateTest data);
+rotateTest rightShifter(rotateTest data);
 
-int main (int agrc, char const * argv [])
+int main (int argc, char const * argv [])
 {
     printInit();
-    test firstTest = {
+
+    rotateTest firstTest = {
         .input = 0xF0000000u,
         .index = 1,
         .rot_cnt = 4
     };
-    test secondTest = {
+
+    rotateTest secondTest = {
         .input = 0x12345678u,
         .index = 2,
         .rot_cnt = 8
     };
-    printPrologue(firstTest);
-    rotateRight(&secondTest);
+
+    printLogue(firstTest);
+
+    rightRun(&secondTest);
 
     return 0;
 }
@@ -43,7 +49,7 @@ void printInit(void)
 
 void printBinary(uint32_t data)
 {
-    for (int i = 31; i >= 0 ; i--)
+    for (int i = BIT_WIDTH-1; i >= 0 ; i--)
     {
         printf("%u", (data>>i)&1u);
         if (!(i%4)) printf(" ");
@@ -51,33 +57,51 @@ void printBinary(uint32_t data)
     printf("\r\n");
 }
 
-void printPrologue(test data)
+void printLogue(rotateTest data)
 {   
     printf("[Init]   Hex: 0x%08X\r\n         Bin: ", data.input);
     printBinary(data.input);
     printf("\r\n");
-    data.input = ((data.input)<<(data.rot_cnt))|((data.input)>>(max_rot-data.rot_cnt));
-    printf("[ROL %d]  Hex: 0x%08X\r\n         Bin: ", data.rot_cnt, data.input);
+    data = leftShifter(data);
+    printf("[ROL %u]  Hex: 0x%08X\r\n         Bin: ", data.rot_cnt, data.input);
     printBinary(data.input);
     printf("         (MSB bits moved to LSB)\r\n");
         printf("\r\n");
-    data.input = ((data.input)>>(data.rot_cnt))|((data.input)<<(max_rot-data.rot_cnt));
-    printf("[ROR %d]  Hex: 0x%08X\r\n         Bin: ", data.rot_cnt, data.input);
+    data = rightShifter(data);
+    printf("[ROR %u]  Hex: 0x%08X\r\n         Bin: ", data.rot_cnt, data.input);
     printBinary(data.input);
     printf("         (Restored to original)\r\n");
     printf("\r\n");
 }
 
-void rotateRight(test* data)
+void leftRun(rotateTest*data)
 {
-    printf("[test %d] Data: 0x%08X -> ",data->index, data->input);
-    data->input = ((data->input)>>(data->rot_cnt))|((data->input)<<(max_rot-data->rot_cnt));
-    printf("ROR %d -> 0x%08X\r\n", data->rot_cnt, data->input);
+    printf("[Test %u] Data: 0x%08X -> ",data->index, data->input);
+    *data = leftShifter(*data);
+    printf("ROL %u -> 0x%08X\r\n", data->rot_cnt, data->input);
 }
 
-void rotateLeft(test* data)
+void rightRun(rotateTest*data)
 {
-    printf("[test %d] Data: 0x%08X -> ",data->index, data->input);
-    data->input = ((data->input)<<(data->rot_cnt))|((data->input)>>(max_rot-data->rot_cnt));
-    printf("ROL %d -> 0x%08X\r\n", data->rot_cnt, data->input);
+    printf("[Test %u] Data: 0x%08X -> ",data->index, data->input);
+    *data = rightShifter(*data);
+    printf("ROR %u -> 0x%08X\r\n", data->rot_cnt, data->input);
+}
+
+rotateTest leftShifter(rotateTest data)
+{
+
+    data.rot_cnt %= BIT_WIDTH;
+    if (data.rot_cnt == 0) return data;
+    data.input = ((data.input)<<(data.rot_cnt))|((data.input)>>(BIT_WIDTH-data.rot_cnt));
+    return data;
+}
+
+rotateTest rightShifter(rotateTest data)
+{
+
+    data.rot_cnt %= BIT_WIDTH;
+    if (data.rot_cnt == 0) return data;
+    data.input = ((data.input)>>(data.rot_cnt))|((data.input)<<(BIT_WIDTH-data.rot_cnt));
+    return data;
 }
